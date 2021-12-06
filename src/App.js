7da586b0a87a84eb90b5dcaf3a7c51f818fbba80
Liam Arzola <liam0215@gmail.com>
2021-12-06 00:32:50 -0500

    fix build errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Router } from "@reach/router";
 
 import Posts from './components/posts'
-import Post from './components/post'
 
 function App() {
   return (
     <Router>
       <Posts path="/" />
-      <Post path="/posts/:id" />
     </Router>
   );
 }
